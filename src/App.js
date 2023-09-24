2cf41158b995a01c6d6fa81c412424fd7e58fb0a
binnama <laasestad@yahoo.no>
2023-09-24 12:50:20 +0200

    Added comp-folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ProductItem from './components/ProductItem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>A cool headline</h1>
+      <h2>Some fancy text</h2>      
+      <ProductItem />
     </div>
   );
 }
---
+++ src/components/ProductItem.js
@@ -0,0 +1,10 @@
+
+function ProductItem() {
+    return (
+        <article>
+            <h2>Blaser R8</h2>
+        </article>
+    );
+}
+
+export default ProductItem;
